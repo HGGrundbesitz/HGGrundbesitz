1a2da6f844f59e5781cd878ed27432903deafc4d
Youssef Slimani <slimaniy151@gmail.com>
2026-04-28 15:31:44 +0200

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,7 +6,6 @@ import Services from '../../components/Services';
 import Process from '../../components/Process';
 import FAQ from '../../components/FAQ';
 import Contact from '../../components/Contact';
-import QuickCheck from '../../components/QuickCheck';
 import Footer from '../../components/Footer';
 import ScrollToTop from '@/components/ScrollToTop';
 
